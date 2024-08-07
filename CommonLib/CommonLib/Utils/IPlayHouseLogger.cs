528da8c252b0901b20c6e38d10d916fa0fc01291
ulala-x <ulala.the.greate@gmail.com>
2024-08-07 14:54:06 +0900

    factor : LOG foramatted 형식만 사용하도록 수정

--- CommonLib/CommonLib/Utils/IPlayHouseLogger.cs
+++ CommonLib/CommonLib/Utils/IPlayHouseLogger.cs
@@ -3,19 +3,9 @@ using System.Runtime.CompilerServices;
 
 namespace PlayHouse.Utils
 {
+
     public interface IPlayHouseLogger
     {
-        void Debug(Func<string> messageFactory, string className, string methodName);
-        void Info(Func<string> messageFactory, string className, string methodName);
-        void Warn(Func<string> messageFactory, string className, string methodName);
-        void Error(Func<string> messageFactory, string className, string methodName);
-        void Trace(Func<string> messageFactory, string className, string methodName);
-        void Fatal(Func<string> messageFactory, string className, string methodName);
-    }
-
-    public interface IPlayHouseFLogger
-    {
-        
         void Debug(Func<FormattableString> messageFactory, string className, string methodName);
         void Info(Func<FormattableString> messageFactory, string className, string methodName);
         void Warn(Func<FormattableString> messageFactory, string className, string methodName);
@@ -35,44 +25,6 @@ namespace PlayHouse.Utils
     }
 
     public class ConsoleLogger : IPlayHouseLogger
-    {
-        public void Trace(Func<string> messageFactory, string className, string methodName)
-        {
-            Console.WriteLine($"{GetTimeStamp()} TRACE: [{className}] ({methodName}) {messageFactory()}");
-        }
-
-        public void Debug(Func<string> messageFactory, string className, string methodName)
-        {
-            Console.WriteLine($"{GetTimeStamp()} DEBUG: [{className}] ({methodName}) {messageFactory()}");
-        }
-
-        public void Info(Func<string> messageFactory, string className, string methodName)
-        {
-            Console.WriteLine($"{GetTimeStamp()} INFO: [{className}] ({methodName}) {messageFactory()}");
-        }
-
-        public void Warn(Func<string> messageFactory, string className, string methodName)
-        {
-            Console.WriteLine($"{GetTimeStamp()} WARN: [{className}] ({methodName}) {messageFactory()}");
-        }
-
-        public void Error(Func<string> messageFactory, string className, string methodName)
-        {
-            Console.WriteLine($"{GetTimeStamp()} ERROR: [{className}] ({methodName}) {messageFactory()}");
-        }
-
-        public void Fatal(Func<string> messageFactory, string className, string methodName)
-        {
-            Console.WriteLine($"{GetTimeStamp()} FATAL: [{className}] ({methodName}) {messageFactory()}");
-        }
-        
-        private string GetTimeStamp()
-        {
-            return DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff");
-        }
-    }
-
-    public class ConsoleFLogger : IPlayHouseFLogger
     {
       public void Trace(Func<FormattableString> messageFactory, string className, string methodName)
         {
@@ -116,7 +68,6 @@ namespace PlayHouse.Utils
 
         public static LogLevel LogLevel { get; private set; } = LogLevel.Debug;
 
-        public static IPlayHouseFLogger FLog { get; private set; } = new ConsoleFLogger();
 
         public static LogLevel FLogLevel { get; private set; } = LogLevel.Debug;
 
@@ -125,171 +76,9 @@ namespace PlayHouse.Utils
             Log = logger;
             LogLevel = logLevel;
         }
-        public static void SetLogger(IPlayHouseFLogger logger, LogLevel logLevel)
-        {
-            FLog = logger;
-            FLogLevel = logLevel;
-        }
     }
-
-    public static class StaticLOG
-    {
-        public static void Trace(Func<string> messageFactory, string className, string methodName)
-        {
-            if (LogLevel.Trace >= LoggerConfigure.LogLevel)
-            {
-                LoggerConfigure.Log.Trace(messageFactory, className, methodName);
-            }
-        }
-
-        public static void Debug(Func<string> messageFactory, string className, string methodName)
-        {
-            if (LogLevel.Debug >= LoggerConfigure.LogLevel)
-            {
-                LoggerConfigure.Log.Debug(messageFactory, className, methodName);
-            }
-        }
-
-        public static void Info(Func<string> messageFactory, string className, string methodName)
-        {
-            if (LogLevel.Info >= LoggerConfigure.LogLevel)
-            {
-                LoggerConfigure.Log.Info(messageFactory, className, methodName);
-            }
-        }
-
-        public static void Warn(Func<string> messageFactory, string className, string methodName)
-        {
-            if (LogLevel.Warning >= LoggerConfigure.LogLevel)
-            {
-                LoggerConfigure.Log.Warn(messageFactory, className, methodName);
-            }
-        }
-
-        public static void Error(Func<string> messageFactory, string className, string methodName)
-        {
-            if (LogLevel.Error >= LoggerConfigure.LogLevel)
-            {
-                LoggerConfigure.Log.Error(messageFactory, className, methodName);
-            }
-        }
-
-        public static void Fatal(Func<string> messageFactory, string className, string methodName)
-        {
-            if (LogLevel.Fatal >= LoggerConfigure.LogLevel)
-            {
-                LoggerConfigure.Log.Fatal(messageFactory, className, methodName);
-            }
-        }
-
-    }
-
-    public static class StaticFLOG
-    {
-
-        public static void Trace(Func<FormattableString> messageFactory, string className, string methodName)
-        {
-            if (LogLevel.Trace >= LoggerConfigure.LogLevel)
-            {
-                LoggerConfigure.FLog.Trace(messageFactory, className, methodName);
-            }
-        }
-
-        public static void Debug(Func<FormattableString> messageFactory, string className, string methodName)
-        {
-            if (LogLevel.Debug >= LoggerConfigure.LogLevel)
-            {
-                LoggerConfigure.FLog.Debug(messageFactory, className, methodName);
-            }
-        }
-
-        public static void Info(Func<FormattableString> messageFactory, string className, string methodName)
-        {
-            if (LogLevel.Info >= LoggerConfigure.LogLevel)
-            {
-                LoggerConfigure.FLog.Info(messageFactory, className, methodName);
-            }
-        }
-
-        public static void Warn(Func<FormattableString> messageFactory, string className, string methodName)
-        {
-            if (LogLevel.Warning >= LoggerConfigure.LogLevel)
-            {
-                LoggerConfigure.FLog.Warn(messageFactory, className, methodName);
-            }
-        }
-
-        public static void Error(Func<FormattableString> messageFactory, string className, string methodName)
-        {
-            if (LogLevel.Error >= LoggerConfigure.LogLevel)
-            {
-                LoggerConfigure.FLog.Error(messageFactory, className, methodName);
-            }
-        }
-
-        public static void Fatal(Func<FormattableString> messageFactory, string className, string methodName)
-        {
-            if (LogLevel.Fatal >= LoggerConfigure.LogLevel)
-            {
-                LoggerConfigure.FLog.Fatal(messageFactory, className, methodName);
-            }
-        }
-    }
-
-    public class LOG<T>
-    {
-        private readonly string _typeName = typeof(T).FullName ?? typeof(T).Name;
-
-        public void Trace(Func<string> messageFactory, [CallerMemberName] string methodName = "")
-        {
-            if (LogLevel.Trace >= LoggerConfigure.LogLevel)
-            {
-                LoggerConfigure.Log.Trace(messageFactory, _typeName, methodName);
-            }
-        }
-
-        public void Debug(Func<string> messageFactory, [CallerMemberName] string methodName = "")
-        {
-            if (LogLevel.Debug >= LoggerConfigure.LogLevel)
-            {
-                LoggerConfigure.Log.Debug(messageFactory, _typeName, methodName);
-            }
-        }
-
-        public void Info(Func<string> messageFactory, [CallerMemberName] string methodName = "")
-        {
-            if (LogLevel.Info >= LoggerConfigure.LogLevel)
-            {
-                LoggerConfigure.Log.Info(messageFactory, _typeName, methodName);
-            }
-        }
-
-        public void Warn(Func<string> messageFactory, [CallerMemberName] string methodName = "")
-        {
-            if (LogLevel.Warning >= LoggerConfigure.LogLevel)
-            {
-                LoggerConfigure.Log.Warn(messageFactory, _typeName, methodName);
-            }
-        }
-
-        public void Error(Func<string> messageFactory, [CallerMemberName] string methodName = "")
-        {
-            if (LogLevel.Error >= LoggerConfigure.LogLevel)
-            {
-                LoggerConfigure.Log.Error(messageFactory, _typeName, methodName);
-            }
-        }
-
-        public void Fatal(Func<string> messageFactory, [CallerMemberName] string methodName = "")
-        {
-            if (LogLevel.Fatal >= LoggerConfigure.LogLevel)
-            {
-                LoggerConfigure.Log.Fatal(messageFactory, _typeName, methodName);
-            }
-        }
-    }
-
-    public class FLOG<T>
+    
+   public class LOG<T>
     {
         private readonly string _typeName = typeof(T).FullName ?? typeof(T).Name;
 
@@ -297,7 +86,7 @@ namespace PlayHouse.Utils
         {
             if (LogLevel.Trace >= LoggerConfigure.LogLevel)
             {
-                LoggerConfigure.FLog.Trace(messageFactory, _typeName, methodName);
+                LoggerConfigure.Log.Trace(messageFactory, _typeName, methodName);
             }
         }
 
@@ -305,7 +94,7 @@ namespace PlayHouse.Utils
         {
             if (LogLevel.Debug >= LoggerConfigure.LogLevel)
             {
-                LoggerConfigure.FLog.Debug(messageFactory, _typeName, methodName);
+                LoggerConfigure.Log.Debug(messageFactory, _typeName, methodName);
             }
         }
 
@@ -313,7 +102,7 @@ namespace PlayHouse.Utils
         {
             if (LogLevel.Info >= LoggerConfigure.LogLevel)
             {
-                LoggerConfigure.FLog.Info(messageFactory, _typeName, methodName);
+                LoggerConfigure.Log.Info(messageFactory, _typeName, methodName);
             }
         }
 
@@ -321,7 +110,7 @@ namespace PlayHouse.Utils
         {
             if (LogLevel.Warning >= LoggerConfigure.LogLevel)
             {
-                LoggerConfigure.FLog.Warn(messageFactory, _typeName, methodName);
+                LoggerConfigure.Log.Warn(messageFactory, _typeName, methodName);
             }
         }
 
@@ -329,7 +118,7 @@ namespace PlayHouse.Utils
         {
             if (LogLevel.Error >= LoggerConfigure.LogLevel)
             {
-                LoggerConfigure.FLog.Error(messageFactory, _typeName, methodName);
+                LoggerConfigure.Log.Error(messageFactory, _typeName, methodName);
             }
         }
 
@@ -337,7 +126,7 @@ namespace PlayHouse.Utils
         {
             if (LogLevel.Fatal >= LoggerConfigure.LogLevel)
             {
-                LoggerConfigure.FLog.Fatal(messageFactory, _typeName, methodName);
+                LoggerConfigure.Log.Fatal(messageFactory, _typeName, methodName);
             }
         }
     }
